55fffd16d4067aa623463196a350291c49cce634
khizarsiddiqui <khizarsiddiqui10@gmail.com>
2022-09-25 03:15:19 +0500

    commit for shader

--- file-12-sq.py
+++ file-12-sq.py
@@ -4,23 +4,18 @@
 # • Using GLSL to write vertex and fragment shaders
 # • Performing texture mapping
 # • Using 3D transformations
-# following simple old-school OpenGL program draws a yellow rectangle on the screen. (example) (step 1)
-import sys
-from OpenGL.GLUT import *
-from OpenGL.GL import *
-def display():
-    glClear (GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-    glColor3f (1.0, 1.0, 0.0)
-    glBegin(GL_QUADS)
-    glVertex3f (-0.5, -0.5, 0.0)
-    glVertex3f (0.5, -0.5, 0.0)
-    glVertex3f (0.5, 0.5, 0.0)
-    glVertex3f (-0.5, 0.5, 0.0)
-    glEnd()
-    glFlush();
-glutInit(sys.argv)
-glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB)
-glutInitWindowSize(400, 400)
-glutCreateWindow("oldgl")
-glutDisplayFunc(display)
-glutMainLoop()
+# Geometric Primitives and 3D Transformation
+# vertex shader
+#version 330 core
+in vec3 aVert;
+uniform mat4 uMVMatrix;
+uniform mat4 uPMatrix;
+out vec4 vCol;
+
+void main() 
+{
+# apply transformations
+    gl_Position = uPMatrix * uMVMatrix * vec4(aVert, 1.0);
+# set color
+    vCol = vec4(1.0, 0.0, 0.0, 1.0);
+}
