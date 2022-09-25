5dc7fb8dac1fb9887f9e581b158b567194416fba
khizarsiddiqui <khizarsiddiqui10@gmail.com>
2022-09-26 02:40:18 +0500

    texture mapping

--- file-12-sq.py
+++ file-12-sq.py
@@ -5,17 +5,46 @@
 # • Performing texture mapping
 # • Using 3D transformations
 # Geometric Primitives and 3D Transformation
-# vertex shader
+# import OpenGL
+# from OpenGL.GL import *
+
+# import numpy, math, sys, os
+# import glutils
+
+import glfw.GLFW as glfw
+
+import OpenGL
+from OpenGL.GL import *
+
+import numpy, math, sys, os
+
+# VERTEX SHADER
 #version 330 core
+
 in vec3 aVert;
+
 uniform mat4 uMVMatrix;
 uniform mat4 uPMatrix;
+
 out vec4 vCol;
 
-void main() 
+void main()
 {
-# apply transformations
+// apply transformations
     gl_Position = uPMatrix * uMVMatrix * vec4(aVert, 1.0);
-# set color
-    vCol = vec4(1.0, 0.0, 0.0, 1.0);
+// set color
+{   vCol = vec4(1.0, 0.0, 0.0, 1.0);
 }
+
+# FRAGMENT SHADER
+#version 330 core
+in vec4 vCol;
+
+out vec4 fragColor;
+void main() {
+// use vertex color
+   fragColor = vCol;
+}
+
+# VERTEX BUFFERS
+# DISPLAYING OPENGL (will be continued)
