712f23dc779f27099ad87dbd699d5f93ff642985
khizarsiddiqui <khizarsiddiqui10@gmail.com>
2022-10-02 02:08:24 +0500

    adding VERTEX shader for GLSL

--- file-12-sq.py
+++ file-12-sq.py
@@ -195,3 +195,31 @@ class Scene:
         glDrawArrays(GL_TRIANGLE_STRIP, 0, 4)
 # unbind VAO
         glBindVertexArray(0)
+
+# vertex shader
+
+strVS = """
+#version 330 core
+
+layout(location = 0) in vec3 aVert;
+
+uniform mat4 uMVMatrix;
+uniform mat4 uPMatrix;
+uniform float uTheta;
+
+out vec2 vTexCoord;
+
+void main() {
+# rotational transform
+        mat4 rot = mat4(
+                        vec4(cos(uTheta), sin(uTheta), 0.0, 0.0),
+                        vec4(-sin(uTheta), cos(uTheta), 0.0, 0.0),
+                        vec4(0.0, 0.0, 1.0, 0.0),
+                        vec4(0.0, 0.0, 0.0, 1.0)
+                        );
+# transform vertex
+        gl_Position = uPMatrix * uMVMatrix * rot * vec4(aVert, 1.0);
+# set texture coordinate
+        vTexCoord = aVert.xy + vec2(0.5, 0.5);
+}
+"""
